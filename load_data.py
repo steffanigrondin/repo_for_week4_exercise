cdbd7b944b63330d90b075eba15da85dc9648439
Steffani Grondin <steffani@telus.net>
2020-10-02 16:33:50 -0400

    defined a function: steffani

--- load_data.py
+++ load_data.py
@@ -2,3 +2,8 @@ import numpy as np
 import pandas as pd
 
 # In this file, you'll put all the functions for loading data.
+
+def dataread(filename):
+    data = pd.read_csv("filename")
+    return data
+
